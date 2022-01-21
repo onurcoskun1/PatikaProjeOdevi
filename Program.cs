87c931a115ad795ce78a3f733ae44e9a6b73d394
onurcoskun1 <coskunonur01@gmail.com>
2022-01-22 00:42:58 +0300

    C# Proje Odevinin Ilki

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("İsminizi Giriniz: ");
+string name = Console.ReadLine();
+Console.WriteLine("Soyadınızı Giriniz: ");
+string surname = Console.ReadLine();
+
+Console.WriteLine("Merhaba " + name + " " + surname);
